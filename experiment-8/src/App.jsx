73c11e9151294d010c925b7256a77af23a42f571
simar2501 <23BAI70318@CUCHD.IN>
2025-09-14 13:40:46 +0000

    committed

---
+++ experiment-8/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LibraryManagement from "./components/LibraryManagement";
+
+function App() {
+  return (
+    <div>
+      <LibraryManagement />
+    </div>
+  );
+}
+
+export default App;
