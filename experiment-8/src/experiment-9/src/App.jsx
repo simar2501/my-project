956fa03b82e3c079b7d3852ff861590e8037c826
simar2501 <23bai70318@cuchd.in>
2025-09-14 17:36:00 +0000

    committed

---
+++ experiment-8/src/experiment-9/src/App.jsx
@@ -0,0 +1,13 @@
+import ClassHierarchyDemo from './components/ClassHierarchyDemo.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ClassHierarchyDemo />
+    </div>
+  );
+}
+
+export default App;
+
